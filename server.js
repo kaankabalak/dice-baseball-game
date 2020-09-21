35e56058f466e77388f7ffde4eb1eaa03a0b8d30
Kaan <kaankabalak@gmail.com>
2020-09-21 02:00:37 -0700

    Fix build command

--- server.js
+++ server.js
@@ -10,10 +10,10 @@ const port = process.env.PORT || 9000
 // Middleware
 app.use(bodyParser.json())
 app.use(cors())
-app.use(express.static(path.join(__dirname, "browser/build")))
+app.use(express.static(path.join(__dirname, "browser")))
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "browser/build", "index.html"))
+  res.sendFile(path.join(__dirname, "browser", "index.html"))
 })
 
 app.listen(process.env.PORT, () => {
