c7a6af7e6601f07555b9e93964af958e4f3df75c
Kaan <kaankabalak@gmail.com>
2020-09-21 01:41:32 -0700

    Integrate React into the project

--- server.js
+++ server.js
@@ -1,15 +1,21 @@
 import "dotenv/config"
+import path from "path"
 import express from "express"
 import bodyParser from "body-parser"
 import cors from "cors"
 
 const app = express()
+const port = process.env.PORT || 9000
 
 // Middleware
 app.use(bodyParser.json())
 app.use(cors())
-app.use(express.static("public"))
+app.use(express.static(path.join(__dirname, "browser/build")))
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "browser/build", "index.html"))
+})
 
 app.listen(process.env.PORT, () => {
-  console.log(`App is listening on port ${process.env.PORT}!`)
+  console.log(`App is listening on port ${port}!`)
 })
